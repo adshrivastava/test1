1894ee1b28442b5ea4c32a422904c10f7907789d
adshrivastava <adshrivastava@users.noreply.github.com>
2024-03-09 13:24:31 +0800

    Update ScriptMkdir.sh

--- ScriptMkdir.sh
+++ ScriptMkdir.sh
@@ -2,11 +2,3 @@
 
 # Create a directory named "a"
 mkdir -p a
-
-# Check if the directory was created successfully
-if [ $? -eq 0 ]; then
-    echo "Directory 'a' created successfully."
-else
-    echo "Failed to create directory 'a'."
-    exit 1  # Exit with non-zero status to indicate failure
-fi
