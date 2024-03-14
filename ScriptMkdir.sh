da365a6ad82fa628e255ad55314de432a38df332
adshrivastava <adshrivastava@users.noreply.github.com>
2024-03-14 16:15:54 +0800

    Update ScriptMkdir.sh

--- ScriptMkdir.sh
+++ ScriptMkdir.sh
@@ -1,4 +1,6 @@
 sudo mkdir -p /home/testadmin/start1
-sudo apt update
-sudo apt install -y openjdk-11-jdk
-sudo mkdir -p /home/testadmin/done1
+#sudo apt update
+#sudo apt install -y openjdk-11-jdk
+#sudo mkdir -p /home/testadmin/done1
+#sudo mkdir /opt/softwareag
+sudo chmod 777 /opt/softwareag
