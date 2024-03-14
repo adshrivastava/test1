4825deae7518d8a612796a71e6e5d42a7af0879d
adshrivastava <adshrivastava@users.noreply.github.com>
2024-03-14 12:11:15 +0800

    Update ScriptMkdir.sh

--- ScriptMkdir.sh
+++ ScriptMkdir.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Create a directory named "b"
-sudo mkdir -p /home/testadmin/abcd
+sudo mkdir -p /home/testadmin/start
 sudo apt install default-jre
 sudo apt install openjdk-11-jre-headless
 sudo apt install openjdk-8-jre-headless
